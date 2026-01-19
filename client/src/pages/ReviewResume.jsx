95e554841b8aeec8007f02bc96ce3864dec3b611
Khushi <khushichauhan2506@gmail.com>
2026-01-19 22:27:48 +0530

    review resume whole done

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,13 +1,49 @@
 import { Edit, Eraser, FileText, Hash, Image, Sparkle } from 'lucide-react'
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios';
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+import Markdown from 'react-markdown'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
+
 
 const ReviewResume =() => {
              // eslint-disable-next-line no-unused-vars
              const [input, setInput] = useState('')
+
+              const [loading, setLoading] = useState(false)
+                   const [content , setContent] = useState("");
+             
+                   const {getToken} = useAuth() 
+             
+
     
               const onSubmitHandler = async(e) =>{
                    e.preventDefault();
+                   try {
+                       setLoading(true);
+                      
+                       const formData = new FormData();
+                       formData.append("resume", input);
+
+                       const {data} = await axios.post("/api/ai/resume-review ", formData, {
+                           headers: {
+                               Authorization: `Bearer ${ await getToken()}`
+                           }
+                       });
+                      
+                       if(data.success){
+                        setContent(data.content); 
+                       }else{
+                        toast.error(data.message);
+                       }
+                   } catch (error) {
+                       toast.error(error.message);
+                   } finally {
+                       setLoading(false);
+                   }
               }
   return (
    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
@@ -28,7 +64,12 @@ const ReviewResume =() => {
 
 
           <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'> 
-           <FileText className='w-5'/>
+
+            {
+              loading ? 
+              <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> :  <FileText className='w-5'/>
+            }
+          
           Review Resume
         </button>
 
@@ -40,7 +81,10 @@ const ReviewResume =() => {
           <FileText className='w-5 h-5 text-[#00DA83]'/>
           <h1 className='text-xl font-semibold'>Analysis Result </h1>
         </div>
-        <div className='flex-1 flex justify-center items-center'>
+         
+         {
+          !content ? (
+                    <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'> 
             <FileText className='w-9 h-9'/>
             <p> Upload Resume to Review </p>
@@ -48,6 +92,17 @@ const ReviewResume =() => {
 
 
         </div>
+          ) :(
+            <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600 '>
+              <div className='reset-tw'>
+                <Markdown>{content}</Markdown>
+              </div>
+            </div>
+          )
+
+         }
+
+
        
       </div>
     </div>
